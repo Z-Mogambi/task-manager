69b92a4bd6ac88652862e32d9ae5d90615fb6fe7
Zach Mogambi <zachariahmogambi1@gmail.com>
2025-08-13 09:59:32 +0300

    Minor changes to AuthController and TaskService

--- src/main/java/com/tm/task_manager/controller/AuthController.java
+++ src/main/java/com/tm/task_manager/controller/AuthController.java
@@ -1,24 +1,47 @@
 package com.tm.task_manager.controller;
 
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.RequestBody;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RestController;
+
+// import com.tm.task_manager.model.User;
+// import com.tm.task_manager.service.UserService;
+
+// @RestController
+// @RequestMapping("/api/auth")
+// public class AuthController {
+
+//     @Autowired 
+//     private UserService userService;
+
+//     @PostMapping("/register")
+//     public String register(@RequestBody User user) {
+//         userService.registerUser(user);
+//         return "User registered successfully!";
+//     }
+// }
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tm.task_manager.model.User;
-import com.tm.task_manager.service.UserService;
+import com.tm.task_manager.repo.UserRepository;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
-
-    @Autowired 
-    private UserService userService;
+    @Autowired private UserRepository userRepository;
+    @Autowired private PasswordEncoder passwordEncoder;
 
     @PostMapping("/register")
-    public String register(@RequestBody User user) {
-        userService.registerUser(user);
-        return "User registered successfully!";
+    public User register(@RequestBody User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
     }
 }
--- src/main/java/com/tm/task_manager/service/TaskService.java
+++ src/main/java/com/tm/task_manager/service/TaskService.java
@@ -1,5 +1,21 @@
 package com.tm.task_manager.service;
 
-public class TaskService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.tm.task_manager.model.Task;
+import com.tm.task_manager.model.User;
+import com.tm.task_manager.repo.TaskRepository;
+import com.tm.task_manager.repo.UserRepository;
+
+@Service
+public class TaskService {
+    @Autowired private TaskRepository taskRepo;
+    @Autowired private UserRepository userRepo;
+
+    public Task createTask(Long userId, Task task) {
+        User user = userRepo.findById(userId).orElseThrow();
+        task.setUser(user);
+        return taskRepo.save(task);
+    }
 }
