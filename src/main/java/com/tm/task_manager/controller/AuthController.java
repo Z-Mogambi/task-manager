b9c251841b5fb7179551a975164b40698ff62c7c
Zachariah Mogambi <166056302+Z-Mogambi@users.noreply.github.com>
2025-09-02 00:53:29 +0300

    Update AuthController.java

--- src/main/java/com/tm/task_manager/controller/AuthController.java
+++ src/main/java/com/tm/task_manager/controller/AuthController.java
@@ -1,30 +1,3 @@
-// package com.tm.task_manager.controller;
-
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.security.crypto.password.PasswordEncoder;
-// import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.RequestBody;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RestController;
-
-// import com.tm.task_manager.model.User;
-// import com.tm.task_manager.repo.UserRepository;
-
-// @RestController
-// @RequestMapping("/api/auth")
-
-// public class AuthController{
-//     @Autowired private UserRepository userRepository;
-
-//     @Autowired private PasswordEncoder passwordEncoder;
-    
-//     @PostMapping("/register")
-//     public User register(@RequestBody User user) {
-//         user.setPassword(passwordEncoder.encode(user.getPassword()));
-//         return userRepository.save(user);
-//     }
-// }
-
 package com.tm.task_manager.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
