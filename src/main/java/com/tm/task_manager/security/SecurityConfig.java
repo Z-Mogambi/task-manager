99523090a56f98f52f798494812d6ea0d7587c81
Zachariah Mogambi <166056302+Z-Mogambi@users.noreply.github.com>
2025-09-02 00:51:21 +0300

    Update SecurityConfig.java

--- src/main/java/com/tm/task_manager/security/SecurityConfig.java
+++ src/main/java/com/tm/task_manager/security/SecurityConfig.java
@@ -1,37 +1,3 @@
-// package com.tm.task_manager.security;
-
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.security.config.Customizer;
-// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-// import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-// import org.springframework.security.crypto.password.PasswordEncoder;
-// import org.springframework.security.web.SecurityFilterChain;
-
-// @Configuration
-// public class SecurityConfig {
-
-//     @Bean
-// public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//     http
-//         .csrf(csrf -> csrf.disable())
-//         .authorizeHttpRequests(auth -> auth
-//             .requestMatchers("/api/auth/register").permitAll()
-//             .requestMatchers("/api/auth/login").permitAll()
-//             .requestMatchers("/h2-console/**").permitAll()
-//             .anyRequest().authenticated()
-//         )
-//         .headers(headers -> headers.frameOptions(frame -> frame.disable()))
-//         .httpBasic(Customizer.withDefaults());
-//     return http.build();
-// }
-
-//     @Bean
-//     public PasswordEncoder passwordEncoder() {
-//         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-//     }
-// }
-
 package com.tm.task_manager.security;
 
 import org.springframework.context.annotation.Bean;
